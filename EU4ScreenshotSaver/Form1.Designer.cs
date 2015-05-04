56d479eb78ca5a3b27c238bc4d6584f018a59b4e
fluzzarn <fluzzarn@gmail.com>
2015-05-04 18:37:59 -0400

    init commit

---
+++ EU4ScreenshotSaver/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace EU4ScreenshotSaver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(88, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(536, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "steam://rungameid/236850";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Game EXE:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(630, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Save Folder:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(88, 46);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(536, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(630, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Browse...";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(88, 73);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(78, 17);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "World Map";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(88, 96);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(80, 17);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.Text = "Screenshot";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(88, 120);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            3600,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(78, 20);
+            this.numericUpDown1.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Delay:";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Checked = true;
+            this.radioButton1.Location = new System.Drawing.Point(88, 146);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(67, 17);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Seconds";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(88, 169);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(62, 17);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Minutes";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(172, 70);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(452, 116);
+            this.textBox3.TabIndex = 13;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(172, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(253, 192);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Stop";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(630, 75);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "Resize Images";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 261);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        public System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ EU4ScreenshotSaver/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+using System.Collections.ObjectModel;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace EU4ScreenshotSaver
+{
+    public partial class Form1 : Form
+    {
+        int picCounter = 0;
+        [DllImport("user32.dll")]
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        [DllImport("user32.dll")]
+        public static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                string file = openFileDialog1.FileName;
+                textBox1.Text = file;
+                string path = Path.GetFullPath(file);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                string file = folderBrowserDialog1.SelectedPath;
+                textBox2.Text = file;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            //Runspace runspace = RunspaceFactory.CreateRunspace();
+            //runspace.Open();
+            //PowerShell ps = PowerShell.Create();
+            //ps.Runspace = runspace;
+            //ps.AddScript((Directory.GetCurrentDirectory() + "\\timelapse.ps1")).AddScript("Start-TimeLapse").AddParameters(new Dictionary<string, string>() { { "exe", textBox1.Text }, { "savedir", textBox2.Text }, { "waiting_time", numericUpDown1.Value.ToString() } });
+            //runspace.SessionStateProxy.SetVariable("TextForm", this);
+            //ps.Invoke(); 
+            
+            //Pipeline pipeline = runspace.CreatePipeline();
+
+            
+            
+
+                Console.WriteLine(Directory.GetCurrentDirectory() + "\\timelapse.ps1");
+                ProcessStartInfo psi = new ProcessStartInfo();
+
+                System.Diagnostics.Process.Start(textBox1.Text);
+               // psi.Arguments = ("-command \"& { . " +"\'"+Directory.GetCurrentDirectory()+ "\\timelapse.ps1\'; StartTimeLapse \'" + textBox1.Text + "\' \'" + textBox2.Text + "\' " + numericUpDown1.Value + "}\"");
+
+                System.Timers.Timer screenTimer = new System.Timers.Timer((double)numericUpDown1.Value * 1000.0);
+                screenTimer.Elapsed += new System.Timers.ElapsedEventHandler(TakeScreenShot);
+                screenTimer.AutoReset = false;
+                screenTimer.Enabled = true;
+            
+
+
+                
+                
+            
+        }
+
+        private void TakeScreenShot(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            System.Timers.Timer screenTimer = new System.Timers.Timer((double)numericUpDown1.Value * 1000.0);
+            screenTimer.Elapsed += new System.Timers.ElapsedEventHandler(TakeScreenShot);
+            screenTimer.AutoReset = false;
+            screenTimer.Enabled = true;
+            IntPtr WindowToFind = FindWindow(null, "Europa Universalis IV");
+            const uint WM_KEYDOWN = 0x100;
+            const uint WM_KEYUP = 0x101;
+            const uint WM_SYSCOMMAND = 0x018;
+            const uint SC_CLOSE = 0x053;
+            IntPtr result3 = SendMessage(WindowToFind, WM_KEYDOWN, ((IntPtr)Keys.F10), (IntPtr)0);
+            IntPtr result2 = SendMessage(WindowToFind, WM_KEYUP, ((IntPtr)Keys.F10), (IntPtr)0);
+            Console.WriteLine("SHOT TAKEN");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            picCounter = 0;
+            string[] files = Directory.GetFiles(textBox2.Text);
+            string[] firstHalf = files.Take(files.Length / 2).ToArray();
+            string[] secondHalf = files.Skip(files.Length / 2).ToArray();
+            Thread resizeThread1 = new Thread(() => resizeImages(firstHalf));
+            resizeThread1.Start();
+
+            //Thread resizeThread2 = new Thread(() => resizeImages(secondHalf));
+            //resizeThread2.Start();
+
+        }
+
+        private void resizeImages(string[] file)
+        {
+            string[] files = Directory.GetFiles(textBox2.Text);
+            
+            Image tempImage;
+            foreach (var image in files)
+            {
+                try
+                {
+                    using (Image picture = Image.FromFile(image))
+                    {
+                        //-16 -13
+                        tempImage = (Image)(new Bitmap(picture, new Size(3942, 1434)));
+                        Graphics textBurn = Graphics.FromImage(tempImage);
+                        string year = image.Substring(image.Length - 16,4);
+                        
+                        textBurn.DrawString(year, new Font(FontFamily.GenericMonospace,72), Brushes.Black, 0, 1300);
+                        
+                        tempImage.Save(textBox2.Text + "\\" + picCounter + ".png");
+                        picCounter++;
+                        textBox3.Invoke(new Action(delegate() { textBox3.AppendText( "\n Resized Image:" + picCounter + '\n'); }));
+                        tempImage.Dispose();
+                        picture.Dispose();
+                        Thread.Sleep(16);
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    System.GC.Collect();
+                    System.GC.WaitForPendingFinalizers();
+                    Console.WriteLine(e.ToString());
+                }
+            }
+
+            textBox3.Invoke(new Action(delegate() { textBox3.AppendText("\n Done on Thread:" + Thread.CurrentThread.ManagedThreadId.ToString() + '\n'); }));
+        }
+    }
+}
